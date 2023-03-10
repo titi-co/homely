231544ed071481c5de3225291065de0580c0e196
Thiago Henrique <thiagohenrique@MacBook-Pro-de-Thiago.local>
2023-03-10 20:17:07 -0300

    feat: firebase auth section

--- lib/src/bloc/authBloc/auth_bloc.dart
+++ lib/src/bloc/authBloc/auth_bloc.dart
@@ -1,5 +1,7 @@
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:homely/src/utils/firebase.dart';
 part 'auth_event.dart';
 part 'auth_state.dart';
 
@@ -7,7 +9,21 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
     on<AuthEvent>((event, emit) async {
       if (event is AppStarted) {
-        emit(AuthUnauthorized());
+        emit(AuthLoading());
+        try {
+          final FirebaseAuth auth = FirebaseAuth.instance;
+
+          if (auth.currentUser == null) {
+            emit(AuthUnauthorized());
+          } else {
+            emit(AuthSuccess());
+          }
+        } on FirebaseAuthException catch (error) {
+          emit(AuthFailure(
+              error: FireBaseUtils().getMessageFromErrorCode(error)));
+        } catch (error) {
+          emit(AuthFailure(error: error.toString()));
+        }
       }
 
       if (event is LoggedIn) {
@@ -18,8 +34,18 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
       if (event is LoggedOut) {
         emit(AuthLoading());
-        // TODO: TOKEN
-        emit(AuthUnauthorized());
+        try {
+          final FirebaseAuth auth = FirebaseAuth.instance;
+
+          auth.signOut();
+
+          emit(AuthUnauthorized());
+        } on FirebaseAuthException catch (error) {
+          emit(AuthFailure(
+              error: FireBaseUtils().getMessageFromErrorCode(error)));
+        } catch (error) {
+          emit(AuthFailure(error: error.toString()));
+        }
       }
     });
   }
